173d89dbdf46e237954a96fe53cc84eb80734f54
diogorodriguees <diogorodrigueslife@gmail.com>
2024-08-24 16:22:21 -0300

    :spaarkles: feat: fix all functions implementeds

--- ArvoreQuadrupla.hs
+++ ArvoreQuadrupla.hs
@@ -1,3 +1,9 @@
+module ArvoreQuadrupla (
+    ArvoreQuadrupla(..),
+    inserirCidade,
+    buscarCidadesProximas,
+    buscarCidadePeloNome
+) where
 
 data ArvoreQuadrupla = Empty |
                         Node {
@@ -10,6 +16,7 @@ data ArvoreQuadrupla = Empty |
                             se::ArvoreQuadrupla
                         } deriving (Show)
 
+-- Metodo de inserir cidade
 inserirCidade :: String -> Double -> Double -> ArvoreQuadrupla -> ArvoreQuadrupla
 inserirCidade name latitude longitude Empty = Node name latitude longitude Empty Empty Empty Empty
 inserirCidade name latitude longitude (Node cityName cityLat cityLon no ne so se)
@@ -18,37 +25,12 @@ inserirCidade name latitude longitude (Node cityName cityLat cityLon no ne so se
     | latitude >= cityLat && longitude < cityLon = Node cityName cityLat cityLon no ne (inserirCidade name latitude longitude so) se
     | otherwise = Node cityName cityLat cityLon no ne so (inserirCidade name latitude longitude se)
 
-printArvoreFormatada:: ArvoreQuadrupla -> IO ()
-printArvoreFormatada Empty = return ()
-printArvoreFormatada (Node city lat lon no ne so se) = do
-    putStrLn $ city
-    printNodeFormatado "NO" no
-    printNodeFormatado "NE" ne
-    printNodeFormatado "SO" so
-    printNodeFormatado "SE" se
 
-printNodeFormatado:: String -> ArvoreQuadrupla -> IO ()
-printNodeFormatado node Empty = do
-    return ()
-
-printNodeFormatado node arvoreQuadrupla = do
-    case arvoreQuadrupla of
-        Node cidade _ _ _ _ _ _ -> do   
-            putStrLn $ " " ++ node ++ ": " ++ cidade
-            printArvoreFormatada arvoreQuadrupla
-        Empty -> do 
-            return () 
-
-getChild:: String -> ArvoreQuadrupla -> ArvoreQuadrupla
-getChild "NO" (Node _ _ _ no _ _ _) = no
-getChild "NE" (Node _ _ _ _ ne _ _) = ne
-getChild "SO" (Node _ _ _ _ _ so _) = so
-getChild "SE" (Node _ _ _ _ _ _ se) = se
-getChild _ Empty = Empty
-
+-- Metodo para calcular distancia entre as ecidades
 calcularDistancia :: (Double, Double) -> (Double, Double) -> Double
 calcularDistancia (lat1, lon1) (lat2, lon2) = sqrt ((lat1 - lat2) ** 2 + (lon1 - lon2) ** 2)
 
+--  Busca cidades dentro do raio informado
 buscarCidadesProximas::Double -> (Double, Double)  -> ArvoreQuadrupla -> [String]
 buscarCidadesProximas _ _ Empty = []
 buscarCidadesProximas raio (latitude, longitude) (Node nomeCidade lat lon no ne so se)
@@ -65,6 +47,7 @@ buscarCidadesProximas raio (latitude, longitude) (Node nomeCidade lat lon no ne 
         buscarCidadesProximas raio (latitude, longitude) so ++
         buscarCidadesProximas raio (latitude, longitude) se
 
+-- Dado um nome, retorna a cidade com aquele nome
 buscarCidadePeloNome:: String -> ArvoreQuadrupla -> Maybe (Double, Double)
 buscarCidadePeloNome _ Empty = Nothing
 buscarCidadePeloNome nome (Node nomeCidade lat lon no ne so se)
@@ -82,17 +65,5 @@ buscarCidadePeloNome nome (Node nomeCidade lat lon no ne so se)
                                 case buscarCidadePeloNome nome se of
                                     Just coordenadas -> Just coordenadas
                                     Nothing -> Just (0, 0)
-
                         
 
-
-main :: IO ()
-main = do
-    let cidades = [("Louisville", 38.2527, -85.7585),
-                   ("Washington", 38.9072, -77.0369),
-                   ("Pittsburgh", 40.4406, -79.9959),
-                   ("New York", 40.7128, -74.0060)]
-
-    let arvoreQuadrupla = foldr(\(name, latitude, longitude) node -> inserirCidade name latitude longitude node) Empty cidades
-    printArvoreFormatada arvoreQuadrupla
-    
--- run.sh
+++ run.sh
@@ -1,2 +1,14 @@
-ghc -o main main.hs
-./main
+cd ./files-compiled
+rm -f *.o
+rm -f main
+rm -f *.hi
+
+cd ..
+ghc -o main Main.hs
+for arquivo in *; do
+  if [ -f "$arquivo" ] && echo "$arquivo" | grep -qv '\.sh$' && echo "$arquivo" | grep -qv '\.hs$'; then
+    mv "$arquivo" "./files-compiled/"
+  fi
+done
+
+./files-compiled/main
